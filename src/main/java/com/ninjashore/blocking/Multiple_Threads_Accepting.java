353ea54002eee52ae7015b2a40657aa3591440bb
Srikanth Lankapalli <srikanth.lankapalli@searshc.com>
2017-05-03 22:00:14 -0500

    Added Multi Thread server.

--- src/main/java/com/ninjashore/blocking/Multiple_Threads_Accepting.java
+++ src/main/java/com/ninjashore/blocking/Multiple_Threads_Accepting.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 /**
  * Created by eng5 on 4/27/17.
  */
-public class SingleThread {
+public class Multiple_Threads_Accepting {
 
     public static void main(String... args) {
 
@@ -16,37 +16,41 @@ public class SingleThread {
         try {
 
             System.out.println("Process ID : " + getPID());
-          //  Thread.sleep(20000);
-
-            System.out.println("Process ID : " + getPID());
             // Create Socket
             ServerSocket ss = new ServerSocket(11111);
 
             while (true) {
 
+               Socket socket =  ss.accept();
+
+                System.out.println("Accepted Connection " + socket.getPort() );
+
+               Thread t = new Thread(new Runnable() {
+                   @Override
+                   public void run() {
+
+                       try {
+
+                           System.out.println("Thread created " + Thread.currentThread().getName());
+                           InputStream in = socket.getInputStream();
+                           OutputStream ou = socket.getOutputStream();
+
+                           int data;
+
+                           while ((data = in.read()) != -1) {
+                               ou.write(data);
+                           }
+                       }catch ( Exception ex){
+                           System.out.println(ex);
+                       }
+
+                   }
+               });
+
+               t.start();
 
             }
-//            Socket socket = ss.accept();
-//
-//
-//            InputStream in = socket.getInputStream();
-//            OutputStream ou = socket.getOutputStream();
-//
-//            int data;
-//            while(true){
-//
-//                while ((data = in.read()) != -1) {
-//
-//                    ou.write(data);
-//                }
-//
-//            }
-//            while ((data = in.read()) != -1) {
-//m,/
-//                ou.write(data);
-//            }
 
-            // socket.getInputStream().close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
